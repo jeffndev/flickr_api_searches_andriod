a443cb52063f50b1661bede907c42e31a3ff037b
Jeff Newell <jeffreynewell1@Jeffreys-MBP.home>
2015-10-19 15:14:39 -0400

    refactored api call, key to manifest

--- app/src/main/java/com/example/jnewel200/resume01/MainActivity.java
+++ app/src/main/java/com/example/jnewel200/resume01/MainActivity.java
@@ -3,26 +3,13 @@ package com.example.jnewel200.resume01;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -48,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
                 final String FORMAT_PARAM = "format";
                 final String NOJSON_CALLBACK_PARAM = "nojsoncallback";
                 final String SEARCH_TEXT_PARAM = "text";
-                String restCallString = "https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=1c4a20853c41e65187fe1ac23eb85538&format=json&nojsoncallback=1";
+                //String restCallString = "https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=1c4a20853c41e65187fe1ac23eb85538&format=json&nojsoncallback=1";
                 Uri buildUri = Uri.parse(BASE_URL).buildUpon()
                         .appendQueryParameter(METHOD_PARAM, "flickr.photos.search")
-                        .appendQueryParameter(API_KEY_PARAM,"1c4a20853c41e65187fe1ac23eb85538")
+                        .appendQueryParameter(API_KEY_PARAM, getString(R.string.flicker_api_key))
                         .appendQueryParameter(FORMAT_PARAM,"json")
                         .appendQueryParameter(NOJSON_CALLBACK_PARAM,"1")
                         .appendQueryParameter(SEARCH_TEXT_PARAM,searchText)
